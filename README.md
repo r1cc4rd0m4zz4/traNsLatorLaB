58f82d082d89c8b1c2578ea9c473093c18d8a727
r1cc4rd0m4zz4 <noreply@example.com>
2024-07-22 23:17:09 +0200

    Add translatorlab.py script and requirements.txt with latest dependencies

--- README.md
+++ README.md
@@ -1,15 +1,78 @@
 # traNsLatorLaB
+The repository contains the source code of the python script, as well as notebooks for using parts extracted from the script via Colaboratory (.ipynb).
 
-## Descrizione
-**traNsLatorLaB** è un modello di traduzione automatica basato su Transformers, addestrato da Meta AI su un grande corpus di testo in diverse lingue. Il modello No Language Left Behind (NLLB) può tradurre da e verso molte lingue diverse, grazie alla sua architettura basata su Transformer.
+## Description
 
-Il codice è scritto in Python utilizzando la libreria Transformers e il framework PyTorch. Il modello è stato addestrato utilizzando tecniche di apprendimento profondo, che gli consentono di ottenere risultati di alta qualità.
+This Python script is designed to facilitate language translation, leveraging advanced machine learning models to accurately translate text from one language to another. It utilizes the `transformers` library to access pre-trained models for high-quality translations. The script supports translating text files or input from the standard input, making it versatile for various use cases.
 
-Il repository contiene il codice sorgente del modello, nonché il notebook che consente di utilizzare il modello tramite Colaboratory. Inoltre, il repository contiene anche la documentazione del modello, che fornisce informazioni dettagliate sull'utilizzo del modello e sulla sua implementazione.
+## Features
 
-## Manleva e Scarico di Responsabilità
-L'utilizzo del modello di traduzione automatica TraNsLatorLaB è a proprio rischio e pericolo. L'autore del codice non si assume alcuna responsabilità per eventuali danni o perdite derivanti dall'utilizzo del modello.
+- **Language Detection**: Automatically detects the language of the input text using both `langdetect` and a fastText model, ensuring accurate translation.
+- **Translation**: Translates text to a specified target language using state-of-the-art models from Helsinki-NLP or Facebook's NLLB-200 distilled model, depending on availability and compatibility.
+- **Device Compatibility**: Automatically detects and utilizes available computational resources, preferring GPU acceleration when available for faster processing.
+- **Customizable Output**: Allows users to specify an output file for the translated text or prints the translation to the terminal if no file is specified.
+- **Sentence Splitting**: Splits input text into manageable sentences or segments to ensure the translation's quality and coherence, especially for longer texts.
 
-Inoltre, l'utilizzo del modello potrebbe essere soggetto a leggi e regolamenti locali o internazionali. È responsabilità dell'utente verificare la conformità dell'utilizzo del modello alle leggi e ai regolamenti applicabili.
+### Dependencies
 
-Infine, l'autore del codice non garantisce la sicurezza del modello o la sua conformità alle normative sulla privacy o sulla sicurezza dei dati. L'utente è responsabile di garantire la sicurezza e la privacy dei propri dati e di utilizzare il modello in conformità alle normative applicabili.
+- Python 3.x
+- `transformers`
+- `torch`
+- `requests`
+- `nltk`
+- `pycountry`
+- `langdetect`
+- `sentencepiece`
+- `fasttext`
+- `sacremoses` (Optional)
+
+### Installing
+
+1. Clone the repository to your local machine.
+2. Install the required Python packages using `pip`:
+
+```bash
+pip install transformers torch requests nltk pycountry langdetect fasttext sentencepiece sacremoses
+```
+or
+```bash
+pip install -r requirements.txt
+```
+
+3. Download and prepare any necessary models or data files as described in the script comments or documentation.
+
+### Executing Program
+
+1. Run the script from the command line, optionally specifying the input text file and target language, by default the script will translate to Italian, example:
+
+```bash
+pbpaste | python translatorlab.py [-o OUTPUT] [-l {it,en}] [txt_path] | pbcopy
+```
+
+2. For direct text input or to use the script in an interactive mode, follow the instructions provided in the script's comments or use the -h flag to access help:
+
+```bash
+python translatorlab.py -h
+```
+
+## Indemnity and disclaimer
+
+Use of the TraNsLatorLaB machine translation template is at your own risk. The author of the code assumes no liability for any damage or loss resulting from the use of the template.
+
+In addition, use of the template may be subject to local or international laws and regulations. It is the user's responsibility to verify that the use of the template complies with applicable laws and regulations.
+
+Finally, the author of the code does not guarantee the security of the template or its compliance with privacy or data security regulations. It is your responsibility to ensure the security and privacy of your data and to use the template in compliance with applicable regulations.
+
+## Acknowledgments
+
+- Helsinki-NLP for the translation models
+- Facebook AI for the NLLB-200 distilled model
+- The transformers library by Hugging Face
+- The PyTorch team for providing an open-source machine learning library for Python
+- The requests library for providing a simple interface for making HTTP requests
+- The Natural Language Toolkit (`nltk`) team for providing essential NLP tools and resources
+- The pycountry library for providing ISO country code utilities and data
+- The langdetect library for providing language detection capabilities
+- The sentencepiece library for providing efficient subword tokenization
+- The fasttext library for providing fast and accurate language identification
+- The sacremoses library for providing tokenization and detokenization utilities
---
+++ translatorlab.py
@@ -0,0 +1,215 @@
+import argparse
+import nltk
+from nltk.tokenize import sent_tokenize
+from nltk.data import find
+import fasttext
+from langdetect import DetectorFactory, detect_langs
+import sys
+import os
+import requests
+from pathlib import Path
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
+import torch
+import pycountry
+
+def get_ft_model():
+    try:
+        # Define the path where you want to save the model
+        model_path = Path.home() / ".fasttext_models/lid.176.ftz" # Faster but less accurate model
+        #model_path = Path.home() / ".fasttext_models/lid218e.bin" # Slower but more accurate model
+        # Check if the model already exists, otherwise download it
+        if not model_path.exists():
+            print("fastText model not found. Starting download...")
+            os.makedirs(model_path.parent, exist_ok=True)  # Create the directory if it does not exist
+            download_file("https://dl.fbaipublicfiles.com/fasttext/supervised-models/lid.176.ftz", model_path) # Fast but less accurate
+            #download_file("https://tinyurl.com/nllblid218e", model_path) # Slower but more accurate
+        # Load the fastText model
+        ft_model = fasttext.load_model(str(model_path))
+    except Exception as e:
+        print(f"Error loading the fastText model: {e}")
+        ft_model = None
+        sys.exit(1)
+    return ft_model
+
+def get_nltk_punkt():
+    try:
+        # Check if the 'punkt' package has already been downloaded
+        find('tokenizers/punkt')
+    except LookupError:
+        try:
+            # Attempt to download the 'punkt' package
+            nltk.download('punkt')
+        except Exception as e:
+            # Handle the download error
+            print(f"Error downloading the 'punkt' package: {e}")
+            sys.exit(1)
+
+def download_file(url, save_path):
+    """
+    Download the file from `url` and save it as `save_path`.
+    """
+    response = requests.get(url)
+    response.raise_for_status()  # Raises an exception for failed responses
+    with open(save_path, "wb") as f:
+        f.write(response.content)
+    print(f"Model downloaded and saved in {save_path}")
+
+def detect_language_fasttext(text:str, ft_model=None):
+    try:
+        if ft_model:
+            predictions = ft_model.predict(text)
+            #print(predictions)
+            return predictions[0][0].split("__label__")[1]
+        else:
+            return None
+    except Exception as e:
+        print(f"Error in fasttext language detection: {e}")
+        return None
+
+def detect_language_langdetect(text):
+    try:
+        DetectorFactory.seed = 0
+        predictions = detect_langs(text)
+        #print(predictions)
+        return predictions[0].lang
+    except Exception as e:
+        print(f"Error in langdetect language detection: {e}")
+        return None
+    
+def split_sentences(text, max_length=512):
+    get_nltk_punkt()
+    sentences = sent_tokenize(text)
+    short_sentences = []
+    for sentence in sentences:
+        while len(sentence) > max_length:
+            # Find a space near the maximum length to split the sentence
+            split_point = sentence.rfind(' ', 0, max_length)
+            if split_point == -1:  # Not found, break at the maximum limit
+                split_point = max_length
+            short_sentences.append(sentence[:split_point])
+            sentence = sentence[split_point:].lstrip()
+        short_sentences.append(sentence)
+    return short_sentences
+
+def detect_language(text, ft_model=None):
+    lang = detect_language_langdetect(text) or detect_language_fasttext(text, ft_model) or "Unknown" 
+    return lang
+
+def save_text(text, base_file_path):
+    """
+    Saves the given text to a file, creating a new file if one already exists.
+    """
+    index = 1
+    file_path = base_file_path
+    while os.path.exists(file_path + ".txt"):
+        file_path = f"{base_file_path}_{index}"
+        index += 1
+    file_path += ".txt"
+    with open(file_path, "w") as file:
+        file.write(text)
+    print(f"Text saved in: {file_path}")
+
+def load_text(base_file_path):
+    """
+    Loads and returns the text from a file if it exists.
+    """
+    if os.path.exists(base_file_path):
+        with open(base_file_path, 'r') as file:
+            text = file.read()
+            return text
+    else:
+        sys.exit(f"The file {base_file_path} does not exist.")
+
+def get_available_device():
+    """
+    Returns the available device for computation ('cuda:0' for GPU, 'cpu' for CPU).
+    """
+    if torch.cuda.is_available():
+        return "cuda:0"
+    #elif torch.backends.mps.is_available():
+    #    # If MPS is available (for Apple devices with M1/M2 chip), use MPS
+    #    return "mps"
+    else:
+        return "cpu"
+
+def convert_language_code(lang_iso639_1):
+    """
+    Converts a two-letter ISO 639-1 language code to a custom format using the three-letter ISO 639-3 code.
+    """
+    lang_iso639_3 = pycountry.languages.get(alpha_2=lang_iso639_1).alpha_3
+    custom_format = f"{lang_iso639_3}_Latn"
+    return custom_format
+
+def translate_text(text, target_lang, device="cpu", ft_model=None):
+    """
+    Translates the given text to the target language using a specified device and fastText model for language detection.
+    """
+    origin_lang = detect_language(text, ft_model)
+    input_lang = convert_language_code(origin_lang)
+    output_lang = convert_language_code(target_lang)
+    model_names = [
+        f'Helsinki-NLP/opus-mt-{origin_lang}-{target_lang}',  # First attempt with a specific model
+        'facebook/nllb-200-distilled-600M'  # Fallback model
+    ]
+    for model_name in model_names:
+        try:
+            if output_lang != input_lang:
+                model = AutoModelForSeq2SeqLM.from_pretrained(model_name).to(device)
+                tokenizer = AutoTokenizer.from_pretrained(model_name)
+                translation_pipeline = pipeline(
+                    'translation',
+                    model=model,
+                    tokenizer=tokenizer,
+                    src_lang=input_lang,
+                    tgt_lang=output_lang,
+                    max_length=512,
+                    device=device
+                )
+                output = translation_pipeline(text)
+                return output[0]['translation_text']
+            else:
+                return text
+        except Exception as e:
+            #print(f"Error during the loading of {model_name}: {e}")
+            continue
+    raise RuntimeError("Unable to load both the specific and fallback models")
+
+def print_in_blocks(text, block_length=800):
+    """
+    Prints the text in blocks of a specified length.
+    """
+    for i in range(0, len(text), block_length):
+        print(text[i:i+block_length])
+        
+def main():
+    """
+    Main function to translate text from a file or standard input to another language.
+    """
+    parser = argparse.ArgumentParser(description="Translates a text to another language.")
+    parser.add_argument("txt_path", nargs='?', default=None, help="The path of the txt file from which to translate text.")
+    parser.add_argument("-o", "--output", help="The path of the output text file. If not specified, the text will be printed to the terminal.")
+    parser.add_argument("-l", "--lang", choices=["it", "en"], default="it", help="The text translate in: 'Italian' (default) or 'English'.")
+    args = parser.parse_args()
+    
+    if not args.txt_path:
+        #text = "Your text here. Come ti chiamo? Je m'appelle Marie. ¿Cómo te llamas? Ich heiße Peter. ¿Y tú? Mi chiamo Giovanni. Et toi?" # Only for test text
+        text = sys.stdin.read()
+    else:
+        text = load_text(args.txt_path)              
+    segments = split_sentences(text)
+    device = get_available_device()
+    ft_model = get_ft_model()
+    translated_segments = []
+    for segment in segments:
+        #print(segment)
+        translated_segment = translate_text(segment, args.lang, device, ft_model)
+        #print(translated_segment)
+        translated_segments.append(translated_segment)
+    text = ' '.join(translated_segments)
+    if args.output:
+        save_text(text, args.output)
+    else:
+        print_in_blocks(text)
+
+if __name__ == "__main__":
+    main()
